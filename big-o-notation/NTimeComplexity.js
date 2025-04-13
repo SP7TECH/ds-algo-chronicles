37ccaaf9b2615dff1520dd750410d1657eb751e5
SP7TECH <spandey247789@gmail.com>
2025-04-14 00:59:33 +0530

    big o

---
+++ big-o-notation/NTimeComplexity.js
@@ -0,0 +1,15 @@
+const arr = ["nemo"];
+
+function findNemo() {
+  let t0 = performance.now();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "nemo") {
+      console.log("nemo found!");
+    }
+  }
+  let t1 = performance.now();
+
+  console.log("Time taken to find nemo was " + (t1 - t0) + " milliseconds");
+}
+
+findNemo();
