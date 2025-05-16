41b4449cc604cbf9af87cd3a10951d21a384d805
SP7TECH <spandey247789@gmail.com>
2025-05-16 09:11:52 +0530

    LinkedList Push methods

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -13,7 +13,25 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    const nodeToPush = new Node(value);
+
+    if (!this.head) {
+      this.head = nodeToPush;
+      this.tail = nodeToPush;
+    } else {
+      this.tail.next = nodeToPush;
+      this.tail = nodeToPush;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 let ll1 = new LinkedList(11);
+console.log(ll1);
+
+ll1.push(4);
 console.log(ll1);
