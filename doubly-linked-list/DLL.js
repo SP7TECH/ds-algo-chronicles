558f61f18ec7a7418eecd7e12b930add4eeef917
SP7TECH <spandey247789@gmail.com>
2025-05-25 15:05:20 +0530

    doubly linked list - push method

--- doubly-linked-list/DLL.js
+++ doubly-linked-list/DLL.js
@@ -13,4 +13,27 @@ class DoublyLinkedList {
     this.tail = newNode;
     this.length = 1;
   }
+
+  push(value) {
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
+
+const list1 = new DoublyLinkedList(11);
+list1.push(3);
+list1.push(23);
+list1.push(7);
+list1.push(4);
+console.log(list1);
