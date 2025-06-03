c364a5cdd774e12d5da5e51b7c8dd579a857e4e2
SP7TECH <spandey247789@gmail.com>
2025-06-03 22:39:16 +0530

    stack - pop method

--- stack/Stack.js
+++ stack/Stack.js
@@ -25,4 +25,26 @@ class Stack {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.length === 0) return undefined;
+
+    let temp = this.top;
+    this.top = this.top.next;
+    temp.next = null;
+
+    this.length--;
+    return temp;
+  }
 }
+
+const stack1 = new Stack(7);
+console.log(stack1);
+
+stack1.push(23);
+stack1.push(3);
+stack1.push(11);
+console.log(stack1);
+
+stack1.pop();
+console.log(stack1);
