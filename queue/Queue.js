767f48a1e07dcbe12fec6a526950c7293666c9e0
SP7TECH <spandey247789@gmail.com>
2025-06-05 01:26:01 +0530

    queue - enqueue

--- queue/Queue.js
+++ queue/Queue.js
@@ -9,7 +9,30 @@ class Queue {
   constructor(value) {
     const newNode = new Node(value);
     this.first = newNode;
-    this.next = newNode;
+    this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
+
+const queue1 = new Queue(11);
+console.log(queue1);
+
+queue1.enqueue(3);
+queue1.enqueue(23);
+queue1.enqueue(7);
+console.log(queue1);
