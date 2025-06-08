bd745d75c2fc5181b2fbe524fb7f51895fc1245a
SP7TECH <spandey247789@gmail.com>
2025-06-08 23:14:03 +0530

    BST - insert

--- trees/BST.js
+++ trees/BST.js
@@ -10,4 +10,51 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) {
+        return undefined;
+      }
+
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+
+        temp = temp.right;
+      }
+    }
+  }
 }
+
+let tree1 = new BST();
+console.log(tree1);
+
+tree1.insert(47);
+tree1.insert(21);
+tree1.insert(76);
+tree1.insert(18);
+tree1.insert(52);
+tree1.insert(82);
+
+console.log(tree1);
+
+tree1.insert(27);
+console.log(tree1);
