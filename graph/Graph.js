fca4f4ac6cdcf4c05c6665fab7308a976c36974a
SP7TECH <spandey247789@gmail.com>
2025-07-16 02:32:43 +0530

    graph - add an edge

--- graph/Graph.js
+++ graph/Graph.js
@@ -11,11 +11,26 @@ class Graph {
 
     return false;
   }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return false;
+    }
+
+    return true;
+  }
 }
 
 const myGraph = new Graph();
-myGraph.addVertex("A");
+myGraph.addVertex("1");
 console.log(myGraph);
 
 // Duplicate
-console.log(myGraph.addVertex("A"));
+console.log(myGraph.addVertex("1"));
+
+// Adding Edge
+myGraph.addVertex("2");
+myGraph.addEdge("1", "2");
+console.log(myGraph);
