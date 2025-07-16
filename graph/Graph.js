91555d9d2df1a895eefbb01ac431c1af154020dd
SP7TECH <spandey247789@gmail.com>
2025-07-16 20:38:55 +0530

    graph - remove edge

--- graph/Graph.js
+++ graph/Graph.js
@@ -21,6 +21,21 @@ class Graph {
 
     return true;
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+
+      return true;
+    }
+
+    return false;
+  }
 }
 
 const myGraph = new Graph();
@@ -34,3 +49,17 @@ console.log(myGraph.addVertex("1"));
 myGraph.addVertex("2");
 myGraph.addEdge("1", "2");
 console.log(myGraph);
+
+const lettersGraph = new Graph();
+lettersGraph.addVertex("A");
+lettersGraph.addVertex("B");
+lettersGraph.addVertex("C");
+
+lettersGraph.addEdge("A", "B");
+lettersGraph.addEdge("B", "C");
+lettersGraph.addEdge("A", "C");
+
+console.log(lettersGraph);
+
+lettersGraph.removeEdge("A", "C");
+console.log(lettersGraph);
